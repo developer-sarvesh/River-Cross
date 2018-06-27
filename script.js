c426b4092ea00e149a6fe6f18a44f57f00bd7234
Sarvesh18 <sarvesh.singh18@hotmail.com>
2018-06-27 13:21:22 +0530

    27-06-2018

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 var right=true;
+var boat="right";
 var count=1;
-var z,add,rem;
+var add,rem;
 var lion_l=0;
 var sheep_l=0;
 var lion_r=3;
 var sheep_r=3;
 
 function play() {
-
   if(sheep_l==3)
   {
     alert("Congrats! You Win");
   }
-  else if(lion_r > sheep_r && lion_l > sheep_l )
+  else if(sheep_r!=0 && lion_r > sheep_r )
   {
-    alert("Sorry! You Loose");
+    alert("Sorry! You Loose R");
+  }
+  else if(sheep_l!=0 && lion_l > sheep_l)
+  {
+    alert("Sorry! You Loose L");
+
   }
 }
+
 function moveBoat() {
-  if(right==true && count!=1)
+  if(right==true && count>1 && count<=3)
   {
+    document.getElementById("btn").disabled = true;
+    var timeOut=setTimeout(function() {
+                                        document.getElementById("btn").disabled = false;
+                                      },2000
+                          );
     right=false;
+    boat="left";
     document.getElementById("b_c").style.right="70%";
     play();
   }
-  else if(right==false && count!=1)
+  else if(right==false && count>1 && count<=3)
   {
+    document.getElementById("btn").disabled = true;
+    var timeOut=setTimeout(function() {
+                                        document.getElementById("btn").disabled = false;
+                                      },2000
+                          );
     right=true;
+    boat="right";
     document.getElementById("b_c").style.right="5%";
     play();
   }
@@ -45,78 +63,81 @@ function add_rem(x,y) {
   add.setAttribute("id",x);
 }
 
-function move(x,y) {
-  if(count==1 || count==2)
+function move(x,y,z) {
+  if(count>=1 && count<3 && z==boat)
   {
-    z="b_c";
-    count++;
+    if(right==true)
+    {
+      z="left";
+      if(y=="lion")
+      {
+        lion_r=lion_r-1;
+      }
+      else
+      {
+        sheep_r=sheep_r-1;
+      }
+    }
+    else
+    {
+      z="right";
+      if(y=="lion")
+      {
+        lion_l=lion_l-1;
+      }
+      else
+      {
+        sheep_l=sheep_l-1;
+      }
+    }
+    count=count+1;
     add_rem(x,y);
-
-    add.setAttribute("onclick", `remove("`+x+`","`+y+`")`);
+    add.setAttribute("onclick", `remove("`+x+`","`+y+`","`+z+`")`);
     add.setAttribute("class", "icon on_boat");
-    document.getElementById(z).insertBefore(add,document.getElementById("b"));
+    document.getElementById("b_c").insertBefore(add,document.getElementById("b"));
+    display(y,z);
   }
   else
   {
-    alert("Only 2 Allowed on Boat");
+    alert("Only 2 Allowed or Invalid Boat Move");
   }
 }
 
-function remove(x,y) {
+function remove(x,y,z) {
   if(right==true)
   {
     z="right";
     if(y=="lion")
     {
       lion_r=lion_r+1;
-      lion_l=lion_l-1;
-
-      console.log("right lion");
-      console.log("ll"+lion_l);
-      console.log("lr"+lion_r);
-      console.log("sl"+sheep_l);
-      console.log("sr"+sheep_r);
     }
     else
     {
       sheep_r=sheep_r+1;
-      sheep_l=sheep_l-1;
-
-      console.log("right sheep");
-      console.log("ll"+lion_l);
-      console.log("lr"+lion_r);
-      console.log("sl"+sheep_l);
-      console.log("sr"+sheep_r);
     }
   }
   else {
     z="left";
     if(y=="lion")
     {
-      lion_r=lion_r-1;
       lion_l=lion_l+1;
-
-      console.log("left lion");
-      console.log("ll"+lion_l);
-      console.log("lr"+lion_r);
-      console.log("sl"+sheep_l);
-      console.log("sr"+sheep_r);
     }
     else
     {
-      sheep_r=sheep_r-1;
       sheep_l=sheep_l+1;
-
-      console.log("left sheep");
-      console.log("ll"+lion_l);
-      console.log("lr"+lion_r);
-      console.log("sl"+sheep_l);
-      console.log("sr"+sheep_r);
     }
   }
   count=count-1;
   add_rem(x,y);
-  add.setAttribute("onclick", `move("`+x+`","`+y+`")`);
+  add.setAttribute("onclick", `move("`+x+`","`+y+`","`+z+`")`);
   add.setAttribute("class", "icon");
   document.getElementById(z).appendChild(add);
+  display(y,z);
+}
+function display(y,z)
+{
+  console.log("---");
+  //console.log(y+"right-"+right+"z-"+z+" "+count);
+  console.log("ll "+lion_l+" lr "+lion_r);
+  console.log("sl "+sheep_l+" sr "+sheep_r);
 }
